74c0ea640e6d50afa2b7951fdc5d5460ad575dd5
Manjit Oberoi <manjit.oberoi@gmail.com>
2017-05-17 14:47:51 +0530

    added src/java files

---
+++ src/D.java
@@ -0,0 +1,8 @@
+public class D{
+	public void printMessage(){
+		System.out.println("###...Message From Master...###");
+		System.out.println("###...Message From Branch1...###");
+		System.out.println("###...Added a line before Stashing...###");
+		System.out.println("###...Tried Stashing...###");
+	}
+}
